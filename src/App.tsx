c1aee511862fafc7c32b588d7757b86830c95e50
Dino Stancic <dinostancic7@gmail.com>
2021-02-08 17:33:59 +0100

    Import loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+// Components
+import LoadingScreen from "./components/LoadingScreen/LoadingScreen";
+
 const App = () => {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <LoadingScreen />
+    </div>
+  );
 };
 
 export default App;
