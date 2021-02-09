b394cb6ab7ff004de114e504d1f86a7ad797970c
Dino Stancic <dinostancic7@gmail.com>
2021-02-09 21:29:09 +0100

    Axios service for genre list

---
+++ src/services/genresServices.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const baseURL: string = `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`;
+
+const getGenres = async () => {
+  const request = axios.get(baseURL);
+  const response = await request;
+  return response.data;
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { getGenres };
