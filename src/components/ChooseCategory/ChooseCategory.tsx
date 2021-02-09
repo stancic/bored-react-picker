4da263ce6b754c68a20a81c58f3cdf16170593cb
Dino Stancic <dinostancic7@gmail.com>
2021-02-09 16:03:16 +0100

    Use context for sharing state and display form

--- src/components/ChooseCategory/ChooseCategory.tsx
+++ src/components/ChooseCategory/ChooseCategory.tsx
@@ -1,7 +1,42 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useEffect, useState } from "react";
+
+// Components
+import LoadingScreen from "../LoadingScreen/LoadingScreen";
+import { TitleStateContext } from "../Context/TitleStateContext";
+
+// Styles
+import "./ChooseCategory.scss";
 
 const ChooseCategory: FunctionComponent = () => {
-  return <div>Hello</div>;
+  const [titleState, setTitleState] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTitleState(false);
+    }, 1000);
+    return () => {
+      setTitleState(true);
+    };
+  }, []);
+  return (
+    <div className="category-choice-container">
+      <TitleStateContext.Provider value={titleState}>
+        <LoadingScreen />
+      </TitleStateContext.Provider>
+      <div className="form-container" style={!titleState ? { opacity: 1 } : {}}>
+        <form action="#">
+          <select name="category" id="category">
+            <option value="action">Action</option>
+            <option value="action2">Action2</option>
+            <option value="action3">Action3</option>
+            <option value="action4">Action4</option>
+          </select>
+          <input type="text" name="year" placeholder="Year" />
+          <input type="submit" value="Find a movie!" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ChooseCategory;
