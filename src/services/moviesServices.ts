6958b3842dba344509e30c74b1389b9fd28ea401
Dino Stancic <dinostancic7@gmail.com>
2021-02-10 16:05:00 +0100

    Change baseURL and add parameters to get api call

--- src/services/moviesServices.ts
+++ src/services/moviesServices.ts
@@ -1,12 +1,13 @@
 import axios from "axios";
 
-const baseURL: string = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`;
+const baseURL: string = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=1`;
 
-const getMovies = async () => {
-  const request = axios.get(baseURL);
+export const getMovies = async (
+  genre: number | undefined,
+  year: number | undefined
+) => {
+  let url = baseURL + `&year=${year}&with_genres=${genre}`;
+  const request = axios.get(url);
   const response = await request;
   return response.data;
 };
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default { getMovies };
