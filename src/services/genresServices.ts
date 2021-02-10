53ca74d3d8d04a32992d01e74ac20cccfb0e3ddc
Dino Stancic <dinostancic7@gmail.com>
2021-02-10 16:04:33 +0100

    Removed default export

--- src/services/genresServices.ts
+++ src/services/genresServices.ts
@@ -2,11 +2,8 @@ import axios from "axios";
 
 const baseURL: string = `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`;
 
-const getGenres = async () => {
+export const getGenres = async () => {
   const request = axios.get(baseURL);
   const response = await request;
   return response.data;
 };
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default { getGenres };
