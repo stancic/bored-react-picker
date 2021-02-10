27f1951192b6ce8fd83e4ed8f4a892785123871a
Dino Stancic <dinostancic7@gmail.com>
2021-02-10 16:00:06 +0100

    Add movie listing component and get shared movies from category component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Components
 import ChooseCategory from "./components/ChooseCategory/ChooseCategory";
+import MoviesListing from "./components/MoviesListing/MoviesListing";
 
 const App = () => {
+  const [movies, setMovies] = useState<any>([]);
+
+  const updateMovies = (movie: []) => {
+    setMovies(movie);
+  };
   return (
     <div className="App">
-      <ChooseCategory />
+      <div className="loading-and-category-container">
+        <ChooseCategory onMoviesUpdate={updateMovies} />
+      </div>
+
+      <div className="movies-container">
+        <MoviesListing movies={movies} />
+      </div>
     </div>
   );
 };
