18f08febd1c745617695e3234c815a514754d5fc
Dino Stancic <dinostancic7@gmail.com>
2021-02-10 16:01:18 +0100

    Change movie initialization and add parameters to get movies request

--- src/components/ChooseCategory/ChooseCategory.tsx
+++ src/components/ChooseCategory/ChooseCategory.tsx
@@ -8,14 +8,16 @@ import React, {
 
 // Components
 import LoadingScreen from "../LoadingScreen/LoadingScreen";
-import { TitleStateContext } from "../Context/TitleStateContext";
+
+// Contexts
+import { TitleStateContext } from "../Context/Context";
 
 // Styles
 import "./ChooseCategory.scss";
 
 // Services
-import genresServices from "../../services/genresServices";
-import moviesServices from "../../services/moviesServices";
+import { getGenres } from "../../services/genresServices";
+import { getMovies } from "../../services/moviesServices";
 
 type DataObject = {
   type: string;
@@ -40,74 +42,107 @@ const moviesReducer = (state: any, action: any) => {
   }
 };
 
-const ChooseCategory: FunctionComponent = () => {
+const ChooseCategory: FunctionComponent<{ onMoviesUpdate: any }> = ({
+  onMoviesUpdate,
+}: any) => {
+  // Reducer states
   const [genresState, genresDispatch] = useReducer(genreReducer, []);
   const [moviesState, moviesDispatch] = useReducer(moviesReducer, []);
 
+  // Used states
   const [genres, setGenres] = useState<{ id: number; name: string }[]>([]);
-  const [selectedGenre, setSelectedGenre] = useState("Action");
-  const [genreID, setGenreID] = useState<number | undefined>(28);
+  const [selectedGenre, setSelectedGenre] = useState("Action"); // Genre on button click
+  const [genreID, setGenreID] = useState<number | undefined>(28); // Genre id from selected genre
 
-  const genreIDRef = useRef(genreID);
+  const [movies, setMovies] = useState<any>([]); // Movies state shared between components
+  const [titleShowState, setTitleShowState] = useState<boolean>(true); // Loading title state
 
-  const [movies, setMovies] = useState<any>([]);
-  const [titleState, setTitleState] = useState<boolean>(true);
+  const [year, setYear] = useState<number>(1895);
+  const [yearHelper, setYearHelper] = useState<number>(1895);
+
+  const [pageHideState, setPageHideState] = useState<boolean>(false);
 
   const initilaizeGenres = async () => {
-    const genres = await genresServices.getGenres();
+    const genres = await getGenres();
     genresDispatch({ type: "initialize", data: genres });
   };
 
-  const initializeMovies = async (genreID?: number) => {
-    const movies = await moviesServices.getMovies();
-    moviesDispatch({ type: "initialize", data: movies.results });
+  const initializeMovies = async (
+    genre: number | undefined,
+    year: number | undefined
+  ) => {
+    if (year && year < 1895 && year > 2021) {
+      alert("WRONG YEAR INPUT");
+    } else {
+      const movies = await getMovies(genre, year);
+      moviesDispatch({ type: "initialize", data: movies.results });
+    }
   };
 
   const chooseGenre = (event: any) => {
     setSelectedGenre(event.target.value);
   };
 
-  const showMovies = (genreID?: number) => {
+  const showMovies = () => {
     if (genres) {
       let selectedGenreID = genres.find(
         (genre) => genre.name === selectedGenre
       );
       setGenreID(selectedGenreID?.id);
     }
+    if (yearHelper < 1895 || yearHelper > 2021) {
+      alert("Year should be later than 1894 and sooner than 2022!");
+    } else {
+      setYear(yearHelper);
+    }
+    //setPageHideState(true);
   };
 
   // Initialize genres and animate form and title
   useEffect(() => {
     initilaizeGenres();
-    initializeMovies();
     setTimeout(() => {
-      setTitleState(false);
+      setTitleShowState(false);
     }, 1000);
     return () => {
-      setTitleState(true);
+      setTitleShowState(true);
     };
   }, []);
 
   // Set movies every time genre is changed
   useEffect(() => {
-    if (genreIDRef) {
-      setMovies(
-        moviesState.filter((movie: any) => movie.genre_ids.includes(genreID))
-      );
-    }
-  }, [moviesState, genreID]);
+    setMovies(moviesState);
+  }, [moviesState]);
 
   // Set genres
   useEffect(() => {
     setGenres(genresState.genres);
   }, [genresState]);
 
+  // Every time genreID changes, movies will change
+  useEffect(() => {
+    initializeMovies(genreID, year);
+  }, [genreID, year]);
+
+  // Share movies to MoviesListing component
+  useEffect(() => {
+    onMoviesUpdate(movies);
+  }, [onMoviesUpdate, movies]);
+
   return (
-    <div className="category-choice-container">
-      <TitleStateContext.Provider value={titleState}>
+    <div
+      className="category-choice-container"
+      style={
+        pageHideState ? { visibility: "hidden", opacity: 0, height: 0 } : {}
+      }
+    >
+      <TitleStateContext.Provider value={titleShowState}>
         <LoadingScreen />
       </TitleStateContext.Provider>
-      <div className="form-container" style={!titleState ? { opacity: 1 } : {}}>
+      <div
+        className="form-container"
+        style={!titleShowState ? { opacity: 1 } : {}}
+      >
         <form action="#">
           <select
             name="category"
@@ -126,8 +161,15 @@ const ChooseCategory: FunctionComponent = () => {
               <option value="">Loading...</option>
             )}
           </select>
-          <input type="text" name="year" placeholder="Year" />
-          <button type="button" onClick={() => showMovies(genreID)}>
+          <input
+            type="number"
+            name="year"
+            placeholder="Year"
+            onChange={({ target }) => {
+              setYearHelper(Number(target.value));
+            }}
+          />
+          <button type="button" onClick={() => showMovies()}>
             FIND MOVIES
           </button>
         </form>
