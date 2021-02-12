e65405f6ce3e3c1b4b4351351366558be15ea847
Dino Stancic <dinostancic7@gmail.com>
2021-02-12 12:27:41 +0100

    Add page changing to data get

--- src/services/moviesServices.ts
+++ src/services/moviesServices.ts
@@ -1,12 +1,13 @@
 import axios from "axios";
 
-const baseURL: string = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=1`;
+const baseURL: string = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=`;
 
 export const getMovies = async (
-  genre: number | undefined,
-  year: number | undefined
+  page: number | undefined,
+  year: number | undefined,
+  genre: number | undefined
 ) => {
-  let url = baseURL + `&year=${year}&with_genres=${genre}`;
+  let url = baseURL + `${page}&year=${year}&with_genres=${genre}`;
   const request = axios.get(url);
   const response = await request;
   return response.data;
