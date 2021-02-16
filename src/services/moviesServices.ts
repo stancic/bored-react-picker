047a0d1d1861796e188853fe5afb7922943a5439
Dino Stancic <dinostancic7@gmail.com>
2021-02-16 14:26:15 +0100

    Add imdb and yt key call

--- src/services/moviesServices.ts
+++ src/services/moviesServices.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const baseURL: string = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=`;
+const detailBaseURL: string = "https://api.themoviedb.org/3/movie/";
 
 export const getMovies = async (
   page: number | undefined,
@@ -8,6 +9,24 @@ export const getMovies = async (
   genre: number | undefined
 ) => {
   let url = baseURL + `${page}&year=${year}&with_genres=${genre}`;
+  const request = axios.get(url);
+  const response = await request;
+  return response.data;
+};
+
+export const getMovieDetail = async (movieID: number | undefined) => {
+  let url =
+    detailBaseURL +
+    `${movieID}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`;
+  const request = axios.get(url);
+  const response = await request;
+  return response.data;
+};
+
+export const getTrailer = async (movieID: number | undefined) => {
+  let url =
+    detailBaseURL +
+    `${movieID}/videos?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`;
   const request = axios.get(url);
   const response = await request;
   return response.data;
