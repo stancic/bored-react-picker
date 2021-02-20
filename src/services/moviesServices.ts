6955be4c155ed3e99aa54e7b38034d9e51a4f349
Dino Stancic <dinostancic7@gmail.com>
2021-02-20 14:07:17 +0100

    Add axios get request for guestID and post request for rating a movie

--- src/services/moviesServices.ts
+++ src/services/moviesServices.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const baseURL: string = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=`;
 const detailBaseURL: string = "https://api.themoviedb.org/3/movie/";
+const guestSessionURL: string = `https://api.themoviedb.org/3/authentication/guest_session/new?api_key=${process.env.REACT_APP_API_KEY}`;
 
 export const getMovies = async (
   page: number | undefined,
@@ -29,5 +30,24 @@ export const getTrailer = async (movieID: number | undefined) => {
     `${movieID}/videos?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`;
   const request = axios.get(url);
   const response = await request;
+  return response.data;
+};
+
+export const getGuestToken = async () => {
+  const request = axios.get(guestSessionURL);
+  const response = await request;
+  return response.data;
+};
+
+export const rateMovie = async (
+  movieID: number | undefined,
+  rating: number | undefined,
+  guestSessionID: string
+) => {
+  let url =
+    detailBaseURL +
+    `${movieID}/rating?api_key=${process.env.REACT_APP_API_KEY}&guest_session_id=${guestSessionID}`;
+  const response = await axios.post(url, { value: rating });
+  console.log(response);
   return response.data;
 };
