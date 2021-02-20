74a559fb8914beaf4b1ac1c400043a0bf51d3511
Dino Stancic <dinostancic7@gmail.com>
2021-02-20 10:49:38 +0100

    Add responsive changes for different screen sizes

--- src/components/MovieDetail/MovieDetail.tsx
+++ src/components/MovieDetail/MovieDetail.tsx
@@ -59,68 +59,76 @@ const MovieDetail: FunctionComponent<Props> = (movie) => {
         }
       />
       <div className="open-movie-container">
-        <h1 className="open-movie-title">{movieDetail.title}</h1>
-        <div className="open-movie-left-side-container">
-          <div className="open-movie-image-and-detail-container">
-            <img
-              className="open-movie-image"
-              src={posterPath + movieDetail.poster_path}
-              alt="Movie"
-            />
-            <div className="open-movie-detail-container">
-              <p className="release-year">
-                Release year: {movieDetail.release_date.substring(0, 4)}
-              </p>
-              <p className="vote-average">
-                Vote average: {movieDetail.vote_average}
-              </p>
+        <div className="open-movie-title-container">
+          <h1 className="open-movie-title">{movieDetail.title}</h1>
+        </div>
+        <div className="open-movie-data-container">
+          <div className="open-movie-left-side-container">
+            <div className="open-movie-image-and-detail-container">
+              <img
+                className="open-movie-image"
+                src={posterPath + movieDetail.poster_path}
+                alt="Movie"
+              />
+              <div className="open-movie-detail-container">
+                <p className="release-year">
+                  Release year: {movieDetail.release_date.substring(0, 4)}
+                </p>
+                <p className="vote-average">
+                  Vote average: {movieDetail.vote_average}
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="open-movie-right-side-container">
-          <div className="open-movie-overview-and-imdbyt-container">
-            <div className="open-movie-overview-container">
-              <p className="movie-overview">{movieDetail.overview}</p>
-            </div>
-            <hr />
-            <div className="open-movie-imdbyt-container">
-              <div className="imdb-container">
-                <a
-                  href={imdbPath + imdbID}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="movie-link"
-                >
-                  <SiImdb />
-                </a>
-                <SiYoutube
-                  className="movie-link"
-                  onClick={openTrailer}
-                  style={
-                    trailerID === undefined
-                      ? { display: "none" }
-                      : { display: "block" }
-                  }
-                />
-                <div
-                  className="trailer-container"
-                  style={
-                    openTrailerFlag ? { display: "none" } : { display: "flex" }
-                  }
-                >
-                  <iframe
-                    title={youTubePath + trailerID}
-                    src={youTubePath + trailerID}
-                    width="auto"
-                    height="auto"
-                    className="embeded-video"
-                    frameBorder="0"
+          <div className="open-movie-right-side-container">
+            <div className="open-movie-overview-and-imdbyt-container">
+              <div className="open-movie-overview-container">
+                <p className="movie-overview">{movieDetail.overview}</p>
+              </div>
+              <hr />
+              <div className="open-movie-imdbyt-container">
+                <div className="imdb-container">
+                  <a
+                    href={imdbPath + imdbID}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="movie-link"
+                  >
+                    <SiImdb />
+                  </a>
+                </div>
+                <div className="yt-container">
+                  <SiYoutube
+                    className="movie-link"
+                    onClick={openTrailer}
                     style={
-                      openTrailerFlag
+                      trailerID === undefined
                         ? { display: "none" }
                         : { display: "block" }
                     }
-                  ></iframe>
+                  />
+                  <div
+                    className="trailer-container"
+                    style={
+                      openTrailerFlag
+                        ? { display: "none" }
+                        : { display: "flex" }
+                    }
+                  >
+                    <iframe
+                      title={youTubePath + trailerID}
+                      src={youTubePath + trailerID}
+                      width="auto"
+                      height="auto"
+                      className="embeded-video"
+                      frameBorder="0"
+                      style={
+                        openTrailerFlag
+                          ? { display: "none" }
+                          : { display: "block" }
+                      }
+                    ></iframe>
+                  </div>
                 </div>
               </div>
             </div>
