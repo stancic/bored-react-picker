53f29d07aa969ffd4d0924a19dd507b969e69d6a
Dino Stancic <dinostancic7@gmail.com>
2021-03-09 10:35:44 +0100

    Inital commit for signup form component

---
+++ src/components/Menu/Signup/SignupForm.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from "react";
+
+// Styles
+import "./SignupForm.scss";
+
+const SignupForm: FunctionComponent = () => {
+  return <div>hello signup</div>;
+};
+
+export default SignupForm;
