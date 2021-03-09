74c3cba55a696977e3a1703c6012e33a1f76a358
Dino Stancic <dinostancic7@gmail.com>
2021-03-09 11:23:56 +0100

    Render signup form on route change

--- src/components/Menu/Signup/SignupForm.tsx
+++ src/components/Menu/Signup/SignupForm.tsx
@@ -1,10 +1,18 @@
 import React, { FunctionComponent } from "react";
 
+// Components
+import MenuData from "../MenuData/MenuData";
+
 // Styles
 import "./SignupForm.scss";
 
 const SignupForm: FunctionComponent = () => {
-  return <div>hello signup</div>;
+  return (
+    <div className="menu-container">
+      <div>hello signup</div>
+      <MenuData />
+    </div>
+  );
 };
 
 export default SignupForm;
