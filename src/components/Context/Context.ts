524cf47025e597569dc3c9401e1dda1d0a8ac69b
Dino Stancic <dinostancic7@gmail.com>
2021-03-09 10:31:39 +0100

    Add contexts' for login and signup form state

--- src/components/Context/Context.ts
+++ src/components/Context/Context.ts
@@ -1,3 +1,5 @@
 import { createContext } from "react";
 
 export const TitleStateContext = createContext(false);
+export const LoginFormContext = createContext(false);
+export const SignupFormContext = createContext(false);
