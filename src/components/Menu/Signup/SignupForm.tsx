30b3607071ab9d28b93e16fe6cc299c276f5dca2
Dino Stancic <dinostancic7@gmail.com>
2021-03-09 12:11:24 +0100

    Add signup form with materialUI

--- src/components/Menu/Signup/SignupForm.tsx
+++ src/components/Menu/Signup/SignupForm.tsx
@@ -2,6 +2,8 @@ import React, { FunctionComponent } from "react";
 
 // Components
 import MenuData from "../MenuData/MenuData";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 // Styles
 import "./SignupForm.scss";
@@ -9,7 +11,32 @@ import "./SignupForm.scss";
 const SignupForm: FunctionComponent = () => {
   return (
     <div className="menu-container">
-      <div>hello signup</div>
+      <div className="signup-form-container user-form-container">
+        <form className="signup-form user-form">
+          <TextField
+            required
+            id="standard-basic"
+            label="Username"
+            className="username input"
+          />
+          <TextField
+            required
+            id="standard-basic"
+            label="Email"
+            className="username input"
+          />
+          <TextField
+            required
+            id="standard-basic"
+            label="Password"
+            type="password"
+            className="password input"
+          />
+          <Button variant="contained" color="default">
+            Login
+          </Button>
+        </form>
+      </div>
       <MenuData />
     </div>
   );
