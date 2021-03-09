8372abcb0416f6644b41016275373fb8c68bb98c
Dino Stancic <dinostancic7@gmail.com>
2021-03-09 12:04:17 +0100

    Add material-ui form for login

--- src/components/Menu/Login/LoginForm.tsx
+++ src/components/Menu/Login/LoginForm.tsx
@@ -2,6 +2,8 @@ import React, { FunctionComponent } from "react";
 
 // Components
 import MenuData from "../MenuData/MenuData";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 // Styles
 import "./LoginForm.scss";
@@ -9,7 +11,26 @@ import "./LoginForm.scss";
 const LoginForm: FunctionComponent = () => {
   return (
     <div className="menu-container">
-      <div>hello login</div>
+      <div className="login-form-container">
+        <form className="login-form">
+          <TextField
+            required
+            id="standard-basic"
+            label="Username/Email"
+            className="usernameEmail input"
+          />
+          <TextField
+            required
+            id="standard-basic"
+            label="Password"
+            type="password"
+            className="password input"
+          />
+          <Button variant="contained" color="default">
+            Login
+          </Button>
+        </form>
+      </div>
       <MenuData />
     </div>
   );
