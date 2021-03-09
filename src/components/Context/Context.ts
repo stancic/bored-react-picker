4dee6784adea1fe4a169ba2b002000a17795c2ab
Dino Stancic <dinostancic7@gmail.com>
2021-03-09 10:58:41 +0100

    Remove context

--- src/components/Context/Context.ts
+++ src/components/Context/Context.ts
@@ -1,5 +1,3 @@
 import { createContext } from "react";
 
 export const TitleStateContext = createContext(false);
-export const LoginFormContext = createContext(false);
-export const SignupFormContext = createContext(false);
