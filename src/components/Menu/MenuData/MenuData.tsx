83a5b8287d844aa78c892d3dec0b6cecf6ba2de4
Dino Stancic <dinostancic7@gmail.com>
2021-03-09 11:24:32 +0100

    Add react-router links to change url for form rendering

--- src/components/Menu/MenuData/MenuData.tsx
+++ src/components/Menu/MenuData/MenuData.tsx
@@ -1,14 +1,23 @@
 import React, { FunctionComponent } from "react";
 
+// Components
+import { Link } from "react-router-dom";
+
 // Styles
 import "./MenuData.scss";
 
 const MenuData: FunctionComponent = () => {
   return (
     <div className="menu-data-container">
-      <h1 className="menu-title">Bored?</h1>
-      <span className="login-open">Login?</span>
-      <span className="signup-open">Sign up?</span>
+      <Link to="/" className="link">
+        <h1 className="menu-title">Bored?</h1>
+      </Link>
+      <Link to="/login" className="link">
+        <span className="login-open">Login?</span>
+      </Link>
+      <Link to="/sign-up" className="link">
+        <span className="signup-open">Sign up?</span>
+      </Link>
     </div>
   );
 };
