d83debc1fb20f33453b724efe390e2cefe60c2a6
Dino Stancic <dinostancic7@gmail.com>
2021-03-10 10:30:36 +0100

    Add error handling

--- src/services/LoginServices.ts
+++ src/services/LoginServices.ts
@@ -3,6 +3,10 @@ const baseurl = "http://localhost:3002/api/users";
 
 export const login = async (credentials: Object) => {
   const url = baseurl + "/login";
-  const response = await axios.post(url, credentials);
-  return response.data;
+  try {
+    const response = await axios.post(url, credentials);
+    return response.data;
+  } catch (error) {
+    return error.response.status;
+  }
 };
