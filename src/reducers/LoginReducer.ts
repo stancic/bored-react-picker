cafe71915a2a10e7e09f1029041496c9dcefd9fa
Dino Stancic <dinostancic7@gmail.com>
2021-03-10 09:32:15 +0100

    Add login and logout reducer

---
+++ src/reducers/LoginReducer.ts
@@ -0,0 +1,34 @@
+import { Dispatch } from "redux";
+import { login } from "../services/LoginServices";
+
+const loginReducer = (state = null, action: any) => {
+  switch (action.type) {
+    case "LOGIN":
+      return action.data.user;
+    case "LOGOUT":
+      return action.data;
+    default:
+      return state;
+  }
+};
+
+export const logUser = (credentials: Object) => {
+  return async (dispatch: Dispatch<any>) => {
+    const user = await login(credentials);
+    dispatch({
+      type: "LOGIN",
+      data: { user },
+    });
+  };
+};
+
+export const logout = () => {
+  return async (dispatch: Dispatch<any>) => {
+    dispatch({
+      type: "LOGOUT",
+      data: null,
+    });
+  };
+};
+
+export default loginReducer;
