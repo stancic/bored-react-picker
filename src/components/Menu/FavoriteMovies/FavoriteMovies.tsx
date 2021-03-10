1299328e20dad48a26cf4467d4143c982395d978
Dino Stancic <dinostancic7@gmail.com>
2021-03-10 17:48:33 +0100

    Initial commit for favorite movies

---
+++ src/components/Menu/FavoriteMovies/FavoriteMovies.tsx
@@ -0,0 +1,14 @@
+import React, { FunctionComponent } from "react";
+
+// Styles
+import "./FavoriteMovies.scss";
+
+const FavoriteMovies: FunctionComponent = () => {
+  return (
+    <div>
+      <div>hello watched</div>
+    </div>
+  );
+};
+
+export default FavoriteMovies;
