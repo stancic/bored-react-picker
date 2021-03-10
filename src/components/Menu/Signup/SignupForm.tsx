2b7a609222c5f97abd4152255bb13d7abc2f6d35
Dino Stancic <dinostancic7@gmail.com>
2021-03-10 11:57:58 +0100

    Add signup functionality

--- src/components/Menu/Signup/SignupForm.tsx
+++ src/components/Menu/Signup/SignupForm.tsx
@@ -1,15 +1,25 @@
 import React, { FunctionComponent, useEffect, useState } from "react";
-
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 // Components
 import MenuData from "../MenuData/MenuData";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 
+// Reducers
+import { userSignup } from "../../../reducers/SignupReducer";
+
 // Styles
 import "./SignupForm.scss";
 
 const SignupForm: FunctionComponent = () => {
+  const dispatch = useDispatch();
+  let redirect = useHistory();
+  const signedUser = useSelector((store: any) => store.signedUser);
   const [showSignupForm, setShowSignupForm] = useState<boolean>(true);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
   useEffect(() => {
     setTimeout(() => {
@@ -19,6 +29,28 @@ const SignupForm: FunctionComponent = () => {
       setShowSignupForm(true);
     };
   }, []);
+
+  const handleSignup = (e: any) => {
+    e.preventDefault();
+    const user = {
+      username: username,
+      email: email,
+      password: password,
+    };
+    dispatch(userSignup(user));
+    setUsername("");
+    setEmail("");
+    setPassword("");
+  };
+
+  useEffect(() => {
+    if (signedUser) {
+      alert("Thanks for signing up");
+      redirect.push("/");
+    } else if (signedUser === 409) {
+      alert("Ooops try again");
+    }
+  }, [signedUser, redirect]);
   return (
     <div className="menu-container">
       <div
@@ -30,12 +62,20 @@ const SignupForm: FunctionComponent = () => {
             required
             id="standard-basic"
             label="Username"
+            value={username}
+            onChange={({ target }) => {
+              setUsername(target.value);
+            }}
             className="username input"
           />
           <TextField
             required
             id="standard-basic"
             label="Email"
+            value={email}
+            onChange={({ target }) => {
+              setEmail(target.value);
+            }}
             className="username input"
           />
           <TextField
@@ -43,6 +83,10 @@ const SignupForm: FunctionComponent = () => {
             id="standard-basic"
             label="Password"
             type="password"
+            value={password}
+            onChange={({ target }) => {
+              setPassword(target.value);
+            }}
             className="password input"
           />
           <Button
@@ -50,6 +94,7 @@ const SignupForm: FunctionComponent = () => {
             color="default"
             className="user-form-submit"
             type="submit"
+            onClick={handleSignup}
           >
             Sign up
           </Button>
