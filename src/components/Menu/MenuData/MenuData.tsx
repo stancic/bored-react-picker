ebe77f5f14ac0d18566df6332a9bc28ace1907b6
Dino Stancic <dinostancic7@gmail.com>
2021-03-10 11:00:46 +0100

    Render hello message if user is logged in

--- src/components/Menu/MenuData/MenuData.tsx
+++ src/components/Menu/MenuData/MenuData.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from "react";
-
+import { useSelector } from "react-redux";
 // Components
 import { Link } from "react-router-dom";
 
@@ -7,16 +7,22 @@ import { Link } from "react-router-dom";
 import "./MenuData.scss";
 
 const MenuData: FunctionComponent = () => {
+  const user = useSelector((store: any) => store.user);
   return (
     <div className="menu-data-container">
+      {user !== 401 && user !== null ? (
+        <span className="login-open hover username">Hello {user.username}</span>
+      ) : (
+        <></>
+      )}
       <Link to="/" className="link">
-        <h1 className="menu-title">Bored?</h1>
+        <h1 className="menu-title hover">Bored?</h1>
       </Link>
       <Link to="/login" className="link">
-        <span className="login-open">Login?</span>
+        <span className="login-open hover">Login?</span>
       </Link>
       <Link to="/sign-up" className="link">
-        <span className="signup-open">Sign up?</span>
+        <span className="signup-open hover">Sign up?</span>
       </Link>
     </div>
   );
