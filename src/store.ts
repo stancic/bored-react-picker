b2c44b33b6ea645f4f318cc04f2c399a6ba242dd
Dino Stancic <dinostancic7@gmail.com>
2021-03-10 11:37:47 +0100

    Add signup reducer

--- src/store.ts
+++ src/store.ts
@@ -2,9 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import loginReducer from "./reducers/LoginReducer";
+import signupReducer from "./reducers/SignupReducer";
 
 const reducer = combineReducers({
   user: loginReducer,
+  signedUser: signupReducer,
 });
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
