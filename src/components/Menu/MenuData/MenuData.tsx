e9137dc9c818b611661ac3aef34f1a8f8c907104
Dino Stancic <dinostancic7@gmail.com>
2021-03-10 12:53:54 +0100

    Add conditional render for logged user

--- src/components/Menu/MenuData/MenuData.tsx
+++ src/components/Menu/MenuData/MenuData.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 // Components
 import { Link } from "react-router-dom";
 
@@ -11,19 +11,31 @@ const MenuData: FunctionComponent = () => {
   return (
     <div className="menu-data-container">
       {user !== 401 && user !== null ? (
-        <span className="login-open hover username">Hello {user.username}</span>
+        <>
+          <span className="open hover username">Hello {user.username}</span>
+          <Link to="/" className="link">
+            <h1 className="menu-title hover">Bored?</h1>
+          </Link>
+          <Link to="/favorite-movies" className="link">
+            <span className="open hover">Favorite movies?</span>
+          </Link>
+          <Link to="/watched-movies" className="link">
+            <span className="open hover">Watched movies?</span>
+          </Link>
+        </>
       ) : (
-        <></>
+        <>
+          <Link to="/" className="link">
+            <h1 className="menu-title hover">Bored?</h1>
+          </Link>
+          <Link to="/login" className="link">
+            <span className="login open hover">Login?</span>
+          </Link>
+          <Link to="/sign-up" className="link">
+            <span className="signup open hover">Sign up?</span>
+          </Link>
+        </>
       )}
-      <Link to="/" className="link">
-        <h1 className="menu-title hover">Bored?</h1>
-      </Link>
-      <Link to="/login" className="link">
-        <span className="login-open hover">Login?</span>
-      </Link>
-      <Link to="/sign-up" className="link">
-        <span className="signup-open hover">Sign up?</span>
-      </Link>
     </div>
   );
 };
