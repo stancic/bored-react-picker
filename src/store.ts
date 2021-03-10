def7d32d542286c3601079183cd414fd55f648b3
Dino Stancic <dinostancic7@gmail.com>
2021-03-10 09:32:56 +0100

    Import login reducer

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+import loginReducer from "./reducers/LoginReducer";
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+  user: loginReducer,
+});
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
