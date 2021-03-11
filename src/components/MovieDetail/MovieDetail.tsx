234fb4436acfe9b650baec172ea0aea1ecb555b9
Dino Stancic <dinostancic7@gmail.com>
2021-03-11 15:46:40 +0100

    Add 'add to favorites' and 'add to watched' buttons

--- src/components/MovieDetail/MovieDetail.tsx
+++ src/components/MovieDetail/MovieDetail.tsx
@@ -1,9 +1,13 @@
 import React, { FunctionComponent, useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 // Components
 import { SiImdb, SiYoutube } from "react-icons/si";
 import { AiOutlineClose } from "react-icons/ai";
 import StarRatingComponent from "react-star-rating-component";
+import Button from "@material-ui/core/Button";
+import StarsIcon from "@material-ui/icons/Stars";
+import VisibilityIcon from "@material-ui/icons/Visibility";
 
 // Styles
 import "./MovieDetail.scss";
@@ -15,10 +19,13 @@ import {
   rateMovie,
 } from "../../services/MoviesServices";
 
-const MovieDetail: FunctionComponent<{
+interface Props {
   movie: any;
   guestSessionID: string;
-}> = ({ movie, guestSessionID }) => {
+}
+
+const MovieDetail: FunctionComponent<Props> = ({ movie, guestSessionID }) => {
+  //movie stuff
   const movieDetail = movie;
   const posterPath = "https://image.tmdb.org/t/p/w200";
   const imdbPath = "https://www.imdb.com/title/";
@@ -29,6 +36,9 @@ const MovieDetail: FunctionComponent<{
   const [movieRatingHelper, setMovieRatingHelper] = useState<number>(0);
   const [rateFlag, setRateFlag] = useState<boolean>(false);
   const [ratedMovieID, setRatedMovieID] = useState<any>(undefined);
+
+  // user stuff
+  const loggedUser = useSelector((store: any) => store.loggedUser);
 
   useEffect(() => {
     const getIMDBID = async () => {
@@ -78,6 +88,30 @@ const MovieDetail: FunctionComponent<{
       <div className="open-movie-container">
         <div className="open-movie-title-container">
           <h1 className="open-movie-title">{movieDetail.title}</h1>
+          {loggedUser && loggedUser.status === 200 ? (
+            <div className="logged-user-buttons-container">
+              <Button
+                aria-label="add to favorites"
+                variant="contained"
+                color="primary"
+                startIcon={<StarsIcon />}
+                className="add-to-favorites-button"
+              >
+                Add to favorites
+              </Button>
+              <Button
+                aria-label="add to watched"
+                variant="contained"
+                color="default"
+                startIcon={<VisibilityIcon />}
+                className="add-to-watched-button"
+              >
+                Add to watched
+              </Button>
+            </div>
+          ) : (
+            <></>
+          )}
         </div>
         <div className="open-movie-data-container">
           <div className="open-movie-left-side-container">
