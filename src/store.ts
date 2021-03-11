fec3ef970e00bf3004a4df84d2fe9c6c1b32b68e
Dino Stancic <dinostancic7@gmail.com>
2021-03-11 10:46:35 +0100

    Add favorite and watched movie reducers

--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,16 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+import favoriteMoviesReducer from "./reducers/FavoriteMoviesReducer";
 import loginReducer from "./reducers/LoginReducer";
 import signupReducer from "./reducers/SignupReducer";
+import watchedMoviesReducer from "./reducers/WatchedMoviesReducer";
 
 const reducer = combineReducers({
   loggedUser: loginReducer,
   signedUser: signupReducer,
+  favoriteMovies: favoriteMoviesReducer,
+  watchedMovies: watchedMoviesReducer,
 });
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
