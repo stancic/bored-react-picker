40e89392bac06d0643be540c168080b16309eb09
Dino Stancic <dinostancic7@gmail.com>
2021-03-11 14:39:07 +0100

    Add remove favorite movie reducer

--- src/reducers/FavoriteMoviesReducer.ts
+++ src/reducers/FavoriteMoviesReducer.ts
@@ -1,7 +1,11 @@
 import { Dispatch } from "redux";
-import { setToken, getFavoriteMovies } from "../services/UserMoviesServices";
+import {
+  setToken,
+  getFavoriteMovies,
+  deleteFavoriteMovie,
+} from "../services/UserMoviesServices";
 
-const favoriteMoviesReducer = (state = null, action: any) => {
+const favoriteMoviesReducer = (state: any = [], action: any) => {
   switch (action.type) {
     case "GET_ALL_FAVORITES":
       if (action.data === undefined) {
@@ -9,6 +13,9 @@ const favoriteMoviesReducer = (state = null, action: any) => {
       } else {
         return action.data;
       }
+    case "REMOVE_FAVORITE":
+      console.log(action);
+      return state.filter((item: any) => item !== state[action.index]);
     default:
       return state;
   }
@@ -25,4 +32,19 @@ export const getAllFavoriteMovies = (userToken: string, userid: string) => {
   };
 };
 
+export const removeMovieFromFavorites = (
+  userToken: string,
+  movieId: string,
+  movieIndex: number
+) => {
+  return async (dispatch: Dispatch<any>) => {
+    setToken(userToken);
+    const deletedFavorite = await deleteFavoriteMovie(movieId);
+    dispatch({
+      type: "REMOVE_FAVORITE",
+      data: deletedFavorite,
+      index: movieIndex,
+    });
+  };
+};
 export default favoriteMoviesReducer;
