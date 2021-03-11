38842574bbe0d40ffafb37980c14f40106005d09
Dino Stancic <dinostancic7@gmail.com>
2021-03-11 09:46:44 +0100

    Initial favorite movies commit

--- src/components/Menu/FavoriteMovies/FavoriteMovies.tsx
+++ src/components/Menu/FavoriteMovies/FavoriteMovies.tsx
@@ -1,12 +1,37 @@
 import React, { FunctionComponent } from "react";
 
+// Components
+import MenuData from "../MenuData/MenuData";
+import Table from "rc-table";
+
 // Styles
 import "./FavoriteMovies.scss";
 
 const FavoriteMovies: FunctionComponent = () => {
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      width: 100,
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+      width: 100,
+    },
+  ];
+  const data = [
+    { name: "Žak", age: 28, address: "some where", key: "1" },
+    { name: "Rose", age: 36, address: "some where", key: "2" },
+  ];
   return (
-    <div>
-      <div>hello watched</div>
+    <div className="menu-container">
+      <div className="watched-movies user-movies-table-container">
+        <Table columns={columns} data={data} />
+      </div>
+      <MenuData />
     </div>
   );
 };
