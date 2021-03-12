2abaddae7fdd0cffb0bb5c6300f59ff752019f6e
Dino Stancic <dinostancic7@gmail.com>
2021-03-12 09:13:28 +0100

    Change services to class

--- src/services/UserMoviesServices.ts
+++ src/services/UserMoviesServices.ts
@@ -7,46 +7,63 @@ export const setToken = (newToken: string) => {
   token = `Bearer ${newToken}`;
 };
 
-export const getFavoriteMovies = async (userid: string) => {
-  const config = {
-    headers: { Authorization: token },
+// Favorites
+export class FavoriteMoviesServices {
+  getFavoriteMovies = async (userid: string) => {
+    const config = {
+      headers: { Authorization: token },
+    };
+    const response = await axios.get(
+      baseurl + "/favorite-movies/" + userid,
+      config
+    );
+    return response.data;
   };
-  const response = await axios.get(
-    baseurl + "/favorite-movies/" + userid,
-    config
-  );
-  return response.data;
-};
 
-export const deleteFavoriteMovie = async (movieid: string) => {
-  const config = {
-    headers: { Authorization: token },
+  addToFavoriteMoives = async (movieToAdd: Object) => {
+    const config = {
+      headers: { Authorization: token },
+    };
+    const response = await axios.post(
+      baseurl + "/favorite-movies",
+      movieToAdd,
+      config
+    );
+    return response.data;
   };
-  const response = await axios.delete(
-    baseurl + "/favorite-movies/" + movieid,
-    config
-  );
-  return response.data;
-};
 
-export const deleteWatchedMovie = async (movieid: string) => {
-  const config = {
-    headers: { Authorization: token },
+  deleteFavoriteMovie = async (movieid: string) => {
+    const config = {
+      headers: { Authorization: token },
+    };
+    const response = await axios.delete(
+      baseurl + "/favorite-movies/" + movieid,
+      config
+    );
+    return response.data;
   };
-  const response = await axios.delete(
-    baseurl + "/watched-movies/" + movieid,
-    config
-  );
-  return response.data;
-};
+}
+// Watched
+export class WatchedMoviesServices {
+  getWatchedMovies = async (userid: string) => {
+    const config = {
+      headers: { Authorization: token },
+    };
+    const response = await axios.get(
+      baseurl + "/watched-movies/" + userid,
+      config
+    );
+    return response.data;
+  };
 
-export const getWatchedMovies = async (userid: string) => {
-  const config = {
-    headers: { Authorization: token },
+  deleteWatchedMovie = async (movieid: string) => {
+    const config = {
+      headers: { Authorization: token },
+    };
+    const response = await axios.delete(
+      baseurl + "/watched-movies/" + movieid,
+      config
+    );
+    return response.data;
   };
-  const response = await axios.get(
-    baseurl + "/watched-movies/" + userid,
-    config
-  );
-  return response.data;
-};
+}
