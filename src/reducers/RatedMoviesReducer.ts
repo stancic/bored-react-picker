e720fbd0d6f2ccb4ad7e552cbbc4b6dd1b706dd9
Dino Stancic <dinostancic7@gmail.com>
2021-03-12 14:17:45 +0100

    Add reducer for movie rating

---
+++ src/reducers/RatedMoviesReducer.ts
@@ -0,0 +1,37 @@
+import { Dispatch } from "redux";
+import {
+  setToken,
+  RatedMoviesServices,
+  MovieToRate,
+} from "../services/RatedMoviesServices";
+
+const ratedMoviesServices = new RatedMoviesServices();
+
+const ratedMoviesReducer = (state: any = [], action: any) => {
+  switch (action.type) {
+    case "RATE_MOVIE":
+      return action.data;
+    default:
+      return state;
+  }
+};
+
+export const rateSelectedMovie = (movieToRate: MovieToRate) => {
+  return async (dispatch: Dispatch<any>) => {
+    const movie = {
+      rate: movieToRate.rate,
+      movieId: movieToRate.movieId,
+      userId: movieToRate.userId,
+    };
+    if (movieToRate.userToken) {
+      setToken(movieToRate.userToken);
+    }
+    const ratedMovie = await ratedMoviesServices.rateMovie(movie);
+    dispatch({
+      type: "RATE_MOVIE",
+      data: ratedMovie,
+    });
+  };
+};
+
+export default ratedMoviesReducer;
