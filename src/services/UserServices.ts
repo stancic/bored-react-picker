86a7fc44dbda19b844cbf05aca7237dd51894afd
Dino Stancic <dinostancic7@gmail.com>
2021-03-15 13:17:21 +0100

    Add interfaces for everything

--- src/services/UserServices.ts
+++ src/services/UserServices.ts
@@ -1,11 +1,41 @@
 import axios from "axios";
 const baseurl = "http://localhost:3002/api/users";
 
-export const login = async (credentials: Object) => {
+// Interfaces
+export interface IUser {
+  id: string;
+  username: string;
+  email: string;
+}
+
+export interface ILoginCredentials {
+  usernameOrEmail: string;
+  password: string;
+}
+
+export interface ISignupCredentials {
+  username: string;
+  email: string;
+  password: string;
+}
+
+export interface IResponse {
+  status: number;
+  user: IUser;
+  message: string;
+  token?: string;
+}
+
+export interface IError {
+  status: number;
+  message: string;
+}
+
+export const login = async (credentials: ILoginCredentials) => {
   const url = baseurl + "/login";
   try {
     const response = await axios.post(url, credentials);
-    const res = {
+    const res: IResponse = {
       status: response.status,
       user: response.data.user,
       message: response.data.message,
@@ -13,7 +43,7 @@ export const login = async (credentials: Object) => {
     };
     return res;
   } catch (error) {
-    const er = {
+    const er: IError = {
       status: error.response.status,
       message: error.response.data.error,
     };
@@ -21,18 +51,18 @@ export const login = async (credentials: Object) => {
   }
 };
 
-export const signup = async (data: Object) => {
+export const signup = async (data: ISignupCredentials) => {
   const url = baseurl + "/sign-up";
   try {
     const response = await axios.post(url, data);
-    const res = {
+    const res: IResponse = {
       status: response.status,
       user: response.data.user,
       message: response.data.message,
     };
     return res;
   } catch (error) {
-    const er = {
+    const er: IError = {
       status: error.response.status,
       message: error.response.data.message,
     };
