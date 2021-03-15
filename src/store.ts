1aaf56650c7647d455931a81179fd6c621085c56
Dino Stancic <dinostancic7@gmail.com>
2021-03-15 13:17:01 +0100

    Add local storage and interface

--- src/store.ts
+++ src/store.ts
@@ -1,13 +1,29 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { LocalStorage } from "./localStorage";
 import thunk from "redux-thunk";
+import throttle from "lodash/throttle";
 import favoriteMoviesReducer from "./reducers/FavoriteMoviesReducer";
 import loginReducer from "./reducers/LoginReducer";
 import ratedMoviesReducer from "./reducers/RatedMoviesReducer";
 import signupReducer from "./reducers/SignupReducer";
 import watchedMoviesReducer from "./reducers/WatchedMoviesReducer";
+import { IError, IUser } from "./services/UserServices";
 
-const reducer = combineReducers({
+// Interfaces
+interface IReducer {
+  loggedUser: IUser | IError;
+  signedUser: IUser | IError;
+  favoriteMovies: any;
+  watchedMovies: any;
+  ratedMovies: any;
+}
+
+const localStorage = new LocalStorage();
+
+const persistedState = localStorage.loadState();
+
+const reducer = combineReducers<IReducer>({
   loggedUser: loginReducer,
   signedUser: signupReducer,
   favoriteMovies: favoriteMoviesReducer,
@@ -15,6 +31,18 @@ const reducer = combineReducers({
   ratedMovies: ratedMoviesReducer,
 });
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(
+  reducer,
+  persistedState,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+store.subscribe(
+  throttle(() => {
+    localStorage.saveState({
+      loggedUser: store.getState().loggedUser,
+    });
+  }, 1000)
+);
 
 export default store;
